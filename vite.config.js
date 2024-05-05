7e907384cdc21778535ed638b1610ac2d6f50032
PT6402 <tranphat701@gmail.com>
2024-05-05 11:24:17 +0700

    upudate

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  build: {
-    outDir: "build",
-  },
-  base: "/",
+  // build: {
+  //   outDir: "build",
+  // },
+  // base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
