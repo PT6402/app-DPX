9ed55fb9959b4af9dbbff47f7fa8737b4ed76d99
PT6402 <tranphat701@gmail.com>
2024-05-15 00:20:36 +0700

    update

--- src/components/pages/FormInforPage/dataSelect.jsx
+++ src/components/pages/FormInforPage/dataSelect.jsx
@@ -1,4 +1,4 @@
-const listType = ["4 chỗ", "7 chỗ", "16 chỗ", "50 chỗ"];
+const listType = ["4 chỗ", "7 chỗ", "16 chỗ", "42 chỗ", "50 chỗ"];
 const handleGenerateOption = ({ list }) => {
   const arr = [];
   list.map((item) => {
